54cc443c25e84ba8c1e18108b55436e27d7144a3
Shuliak Liubov <shuliak.lyubov@gmail.com>
2022-03-15 17:27:09 +0200

    partials

--- server.js
+++ server.js
@@ -1,19 +1,39 @@
-const { request, response } = require("express");
 const express = require("express");
+const cookieSession = require("cookie-session");
 const path = require("path");
+
+const FeedbackService = require("./services/FeedbackService");
+const SpeakersService = require("./services/SpeakerService");
+
+const feedbackService = new FeedbackService("./data/feedback.json");
+const speakersService = new FeedbackService("./data/speakers.json");
+
+const routes = require("./routes");
+
 const app = express();
 
-const port = 3000;
+const port = 8000;
+
+app.set("trust proxy", 1);
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["xcsfsd", "scdaa"],
+  })
+);
 
 app.use(express.static(path.join(__dirname, "./static")));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
 
-app.get("/", (request, response) => {
-  response.sendFile(path.join(__dirname, "./static/index.html"));
-});
-app.get("/speakers", (request, response) => {
-  response.sendFile(path.join(__dirname, "./static/speakers.html"));
-});
+app.use(
+  "/",
+  routes({
+    feedbackService,
+    speakersService,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Express to port ${port}`);
